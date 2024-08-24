56f327dfe7d95322d7ee06e9c21c9238ce75ac55
Kei-K23 <keiksl2301@gmail.com>
2024-08-24 21:37:09 +0630

    Add backend server struct

--- main.go
+++ main.go
@@ -1,5 +1,26 @@
 package main
 
+import "sync"
+
+type Server struct {
+	Address     string
+	Alive       bool
+	Mu          sync.RWMutex
+	Connections int
+}
+
+func (s *Server) IsAlive() bool {
+	s.Mu.RLock()
+	defer s.Mu.RUnlock()
+	return s.Alive
+}
+
+func (s *Server) SetAlive(alive bool) {
+	s.Mu.Lock()
+	defer s.Mu.Unlock()
+	s.Alive = alive
+}
+
 func main() {
 
 }
