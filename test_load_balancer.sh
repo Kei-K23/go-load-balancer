81d2aab771559bb3a5dcf86c206ceb8ec6752e71
Kei-K23 <keiksl2301@gmail.com>
2024-08-25 10:38:42 +0630

    Add shell script to test load balancer

---
+++ test_load_balancer.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# URL of your load balancer
+url="http://localhost:8080"
+
+# Number of requests to send
+requests=5000
+
+# Log the start time
+start_time=$(date +%s)
+
+echo "Sending $requests requests to $url..."
+
+# Send requests in parallel using &
+for i in $(seq 1 $requests); do
+    curl -s $url &
+done
+
+# Wait for all background jobs to finish
+wait
+
+# Log the end time
+end_time=$(date +%s)
+
+# Calculate the total time taken
+total_time=$((end_time - start_time))
+
+echo "Sent $requests requests to $url"
+echo "Total time: $total_time seconds"
