a8cf4d4c586743019e3f0e9ac9616d6d3939ce6e
Kei-K23 <keiksl2301@gmail.com>
2024-08-25 09:48:55 +0630

    Add backend servers and use Go Gin framework

---
+++ server3/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	SERVER_ADDR = "0.0.0.0:8083"
+)
+
+func main() {
+	r := gin.Default()
+	r.GET("/health", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": fmt.Sprintf("This is health response from %s", SERVER_ADDR),
+		})
+	})
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+			"address": SERVER_ADDR,
+		})
+	})
+	r.Run(SERVER_ADDR)
+}
